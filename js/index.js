17557501107bbd2881a0e3693da3efd7a027baef
AnareAnare <122323178+AnareAnare@users.noreply.github.com>
2023-03-05 02:56:06 +0300

    first

---
+++ js/index.js
@@ -0,0 +1,23 @@
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.getElementsByTagName('html')[0];
+    const burger = document.querySelector('.burger');
+    const closeMenu = document.querySelector('.btn__close');
+    const menu = document.querySelector('.header__nav');
+    const menuLinks = menu.querySelectorAll('.nav__link');
+    burger.addEventListener('click', () => {
+      menu.classList.add('header__nav_active');
+      html.classList.add('stop-scroll');
+    });
+    closeMenu.addEventListener('click', () => {
+      menu.classList.remove('header__nav_active');
+      html.classList.remove('stop-scroll');
+    });
+    menuLinks.forEach(el => {
+      el.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+        html.classList.remove('stop-scroll');
+      });
+    });
+  });
+})();
